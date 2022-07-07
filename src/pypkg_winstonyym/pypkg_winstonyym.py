dc114e17cf2116be1cc1b1e0e1abf3516253a387
winstonyym <winstonyym@u.nus.edu>
2022-07-07 12:19:20 +0800

    build: add dependencies

---
+++ src/pypkg_winstonyym/pypkg_winstonyym.py
@@ -0,0 +1,26 @@
+from string import punctuation
+from collections import Counter
+
+def load_text(input_file):
+        """Load text from a text file and return as a string."""
+        with open(input_file, "r") as file:
+            text = file.read()
+        return text
+
+
+def clean_text(text):
+        """Lowercase and remove punctuation from a string."""
+        
+        text = text.lower()
+        for p in punctuation:
+            text = text.replace(p, "")
+        return text
+
+
+def count_words(input_file):
+        """Count unique words in a string."""
+        
+        text = load_text(input_file)
+        text = clean_text(text)
+        words = text.split()
+        return Counter(words)
