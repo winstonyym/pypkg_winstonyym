b89299945638ed67d7f6e347d8c863d17fc26e44
winstonyym <winstonyym@u.nus.edu>
2022-07-08 11:54:31 +0800

    build: add tests and dev package

--- tests/test_pypkg-winstonyym.py
+++ tests/test_pypkg-winstonyym.py
@@ -1,1 +1,15 @@
 from pypkg_winstonyym import pypkg_winstonyym
+from collections import Counter
+
+
+
+def test_count_words():
+    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+            'the': 1, 'same': 1, 'thing': 1, 
+            'over': 2, 'and': 2, 'expecting': 1,
+            'different': 1, 'results': 1})
+
+    actual = pypkg_winstonyym.count_words('tests/quote.txt')
+    assert expected == actual, 'Counted incorrectly!'
+
+
